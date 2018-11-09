1d05a7111ce34d01e020974b115fd87b26899c74
samuelololol <samuelololol@gmail.com>
2018-11-09 12:03:21 +0800

    Update pyctags, remove mkl

--- dev-python/pyctags/files/pyctags.sh
+++ dev-python/pyctags/files/pyctags.sh
@@ -18,7 +18,34 @@ if [ ! -f ~/.ctagsignore ]; then
 test" > ~/.ctagsignore
 fi
 
-rm -rf ./tags
+if [ -f tags ]; then
+    if [ $2 = "-f" ]; then
+        printf "Remove previous tag file\n"
+        rm -rf tags
+        printf "tags are removed\n"
+    else
+        # remove previous
+        if [ -n "$ZSH_VERSION"  ]; then
+            printf "Would you like to remove previous tag file"
+            read -r " ? [Y/N]" response
+        else
+            read -r -p " Would you like to remove previous tag file? [Y/N]" response
+        fi
+
+        # removing old tags
+        case "$response" in
+            [yY][eE][sS]|[yY])
+                rm -rf tags
+                printf "tags are removed\n"
+                ;;
+            *)
+                printf "tags are keeped\n"
+                ;;
+        esac
+    fi
+fi
+
+
 ctags \
 -R \
 -h ".py" \
